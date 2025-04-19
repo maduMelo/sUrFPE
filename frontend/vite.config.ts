fc0fa408900fba5675312414c4ab6aaefd58b525
madu5 <MM218@outlook.com.br>
2025-04-19 11:29:57 -0300

    Revert "feat: setup shadcn"
    This reverts commit 532ad5d004c93b1338d01cb532e5009fc31d8930.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
-import path from "path";
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
