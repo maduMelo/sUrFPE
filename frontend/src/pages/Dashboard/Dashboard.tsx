95c115bc463e31bae9764e89737a1dfa2adecdf2
madu5 <MM218@outlook.com.br>
2025-04-27 14:55:16 -0300

    feat: add grid to organize charts

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -50,74 +50,86 @@ export const Dashboard = ({ }) => {
                     <Button
                         variant='outlined'
                         callToAction='Baixar Análise'
-                        />
+                    />
                 </div>
             </header>
 
-            <div className='flex flex-wrap justify-between w-full gap-y-4'>
-                <ChartContainer
-                    title="Desempenho geral"
-                    description="Média geral das manobras praticadas pelo atleta"
-                    width={22}
-                >
-                    <TricksMean
-                        tricks={analysisData["tricks_mean_scores"]["tricks"]}
-                        tricksScores={analysisData["tricks_mean_scores"]["scores"]}
-                    />
-                </ChartContainer>
+            {/* Charts Grid */}
+            <div className='h-full w-full grid grid-cols-4 grid-rows-5 gap-4'>
+                {/* Desempenho geral */}
+                <div className="col-span-1 row-span-1 rounded-lg">
+                    <ChartContainer
+                        title="Desempenho geral"
+                        description="Média geral das manobras praticadas pelo atleta"
+                    >
+                        <TricksMean
+                            tricks={analysisData["tricks_mean_scores"]["tricks"]}
+                            tricksScores={analysisData["tricks_mean_scores"]["scores"]}
+                        />
+                    </ChartContainer>
+                </div>
 
-                <ChartContainer
-                    title="Total de manobras"
-                    description="Número total de manobras praticadas detalhadas por lado da onda"
-                    width={22}
-                >
-                    <TricksCount
-                        tricksCount={analysisData["wave_type_tricks_count"]["counts"]}
-                    />
-                </ChartContainer>
+                {/* Total de manobras */}
+                <div className="col-span-1 row-span-1 rounded-lg">
+                    <ChartContainer
+                        title="Total de manobras"
+                        description="Número total de manobras praticadas detalhadas por lado da onda"
+                    >
+                        <TricksCount
+                            tricksCount={analysisData["wave_type_tricks_count"]["counts"]}
+                        />
+                    </ChartContainer>
+                </div>
 
-                <ChartContainer
-                    title="Feedback do treinamento"
-                    description="Feedback gerado por IA a partir das análises feitas em cima dos dados do treinamento"
-                    width={45}
-                >
-                    <WaveTypeTricks
-                        tricks={analysisData["tricks_performance_by_wave_type"]["tricks"]}
-                        frontsideScores={analysisData["tricks_performance_by_wave_type"]["frontside"]}
-                        backsideScores={analysisData["tricks_performance_by_wave_type"]["backside"]}
-                    />
-                </ChartContainer>
+                {/* Feedback do treinamento */}
+                <div className="col-span-2 row-span-2 rounded-lg">
+                    <ChartContainer
+                        title="Feedback do treinamento"
+                        description="Feedback gerado por IA a partir das análises feitas em cima dos dados do treinamento"
+                    >
+                        <WaveTypeTricks
+                            tricks={analysisData["tricks_performance_by_wave_type"]["tricks"]}
+                            frontsideScores={analysisData["tricks_performance_by_wave_type"]["frontside"]}
+                            backsideScores={analysisData["tricks_performance_by_wave_type"]["backside"]}
+                        />
+                    </ChartContainer>
+                </div>
 
-                <ChartContainer
-                    title="Desempenho por manobra"
-                    description="Média geral de cada manobra detalhando o lado da onda"
-                    width={45}
-                >
-                    <WaveTypeTricks
-                        tricks={analysisData["tricks_performance_by_wave_type"]["tricks"]}
-                        frontsideScores={analysisData["tricks_performance_by_wave_type"]["frontside"]}
-                        backsideScores={analysisData["tricks_performance_by_wave_type"]["backside"]}
-                    />
-                </ChartContainer>
+                {/* Desempenho por manobra */}
+                <div className="col-span-2 row-span-1 rounded-lg">
+                    <ChartContainer
+                        title="Desempenho por manobra"
+                        description="Média geral de cada manobra detalhando o lado da onda"
+                    >
+                        <WaveTypeTricks
+                            tricks={analysisData["tricks_performance_by_wave_type"]["tricks"]}
+                            frontsideScores={analysisData["tricks_performance_by_wave_type"]["frontside"]}
+                            backsideScores={analysisData["tricks_performance_by_wave_type"]["backside"]}
+                        />
+                    </ChartContainer>
+                </div>
 
-                <ChartContainer
-                    title="Desempenho por indicador"
-                    description="Média geral de cada indicador das manobras detalhados por lado da onda"
-                    width={92}
-                >
-                    <>
-                        {analysisData["indicator_trick_means_scores"]["tricks"].map((trick, index) => (
-                            <div className='flex flex-col mt-10 items-center gap-4' key={index}>
-                                <h1 className='text-[15px] text-[#224A68] font-medium'>
-                                    {trick}
-                                </h1>
-                                <TrickMetrics
-                                    trickMetrics={analysisData["indicator_trick_means_scores"]["chartsInfo"][index]}
-                                />
-                            </div>
-                        ))}
-                    </>
-                </ChartContainer>
+                {/* Desempenho por indicador */}
+                <div className="col-span-4 row-span-3 rounded-lg">
+                    <ChartContainer
+                        title="Desempenho por indicador"
+                        description="Média geral de cada indicador das manobras detalhados por lado da onda"
+                    >
+                        <>
+                            {analysisData["indicator_trick_means_scores"]["tricks"].map((trick, index) => (
+                                <div className='flex flex-col mt-10 items-center gap-4' key={index}>
+                                    <h1 className='text-[15px] text-[#224A68] font-medium'>
+                                        {trick}
+                                    </h1>
+                                    <TrickMetrics
+                                        trickMetrics={analysisData["indicator_trick_means_scores"]["chartsInfo"][index]}
+                                    />
+                                </div>
+                            ))}
+                        </>
+                    </ChartContainer>
+                </div>
+
             </div>
         </div>
     );
