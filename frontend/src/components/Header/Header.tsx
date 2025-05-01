f130f3e448a604aaf0ef0054453ab55a437fb104
madu5 <MM218@outlook.com.br>
2025-05-01 18:42:36 -0300

    feat: adjustments on header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.png";
 export const Header = () => {
 
     return (
-        <header className="bg-white shadow-md p-3 mb-6 rounded-br-[40px]">
+        <header className="fixed top-0 left-0 z-40 flex items-center bg-white shadow-md pl-3 h-18 w-full rounded-br-[40px]">
             <img src={logo} alt="sUrFPE logo" />
         </header>
     );
