21efc0ba598eb8db7b329e5f7025e37de18b6983
madu5 <MM218@outlook.com.br>
2025-05-01 18:42:46 -0300

    feat: add footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import logo from "../../assets/logo-white.png";
+import Instagram from "../../assets/ig-icon.png";
+
+
+export const Footer = () => {
+
+    return (
+        <footer
+            className="flex items-center justify-between px-10 h-28 mt-[3rem]  bg-[#224A68] text-white overflow-clip"
+        >
+            <img src={logo} alt="sUrFPE logo" className="w-[5rem]" />
+
+            <div className="text-[12px]">
+                sUrFPE {new Date().getFullYear()} © Todos os direitos reservados
+            </div>
+
+            <a 
+                href="https://www.instagram.com/surfpe_" target="blank"
+                className="hover:bg-white/20 rounded-md cursor-pointer"
+            >
+                <img src={Instagram} alt="sUrFPE logo" className="w-[2.5rem]" />
+            </a>
+        </footer>
+    );
+};
